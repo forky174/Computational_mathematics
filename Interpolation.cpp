c82a148a76c718af4a278ded5bc4dee816b5b102
forky174 <forky174@gmail.com>
2024-10-28 12:30:52 +0300

    Fixed Interpolation.cpp

--- Interpolation.cpp
+++ Interpolation.cpp
@@ -1,5 +1,6 @@
 #include <array>
 #include <iostream>
+#include <algorithm>
 #include <cmath>
 #define _USE_MATH_DEFINES
 
@@ -34,7 +35,7 @@ long double fun(long double x) {
 };
 
 int main () {
-    const long unsigned int N = 3;
+    const long unsigned int N = 5;
     std::array<std::pair<long double, long double>, 6> line_segments {std::make_pair(0., 2.),    std::make_pair(0., 1.),
                                                                       std::make_pair(0., 1./2.), std::make_pair(0., 1./4.),
                                                                       std::make_pair(0., 1./8.), std::make_pair(0., 1./16.)};
@@ -63,7 +64,7 @@ int main () {
         long double b = line_segments[i].second;
         // std::cout << "Отрезок " <<  b - a << std::endl;
         for (unsigned long j = N; j >= 1; j--) {
-            Chebyshev_nodes[i][j-1] = (a+b)/2 + (b-a)/2 * std::cos(M_PI * (2*j - 1) / (2*N)); // проверить !!! (стр.13 конспектов)
+            Chebyshev_nodes[i][j-1] = (a+b)/2 + (b-a)/2 * std::cos(M_PI * (2*j - 1) / (2*N));
             // std::cout << Chebyshev_nodes[i][j-1] << " ";
         }
         // std::cout << std::endl;
@@ -87,12 +88,19 @@ int main () {
         NewtonInterpolation<long double, long double, N> Uniform_distribution;
         NewtonInterpolation<long double, long double, N> Chebyshev;
 
+        std::reverse(Uniform_distribution_nodes[i].begin(), Uniform_distribution_nodes[i].end());
+        std::reverse(Uniform_distribution_values[i].begin(), Uniform_distribution_values[i].end());
+
         Uniform_distribution.NewtonInterpolator(Uniform_distribution_nodes[i], Uniform_distribution_values[i]);
         Chebyshev.NewtonInterpolator(Chebyshev_nodes[i], Chebyshev_values[i]);
 
         for (unsigned long int p = 0; p < 1000; ++p) {
+            // std::cout << x << std::endl;
             Uniform_distribution_inter_value = Uniform_distribution.interpolate(x);
             Chebyshev_inter_value = Chebyshev.interpolate(x);
+            // std::cout << "Равномерное распределение на отрезке " << length << " :: " << Uniform_distribution_inter_value << std::endl;
+            // std::cout << "Чебышевское распределение на отрезке " << length << " :: " << Chebyshev_inter_value << std::endl;
+            // std::cout << "Значения функции на отрезке          " << length << " :: " << fun(x) << std::endl;
             if (std::abs(Uniform_distribution_inter_value - fun(x)) > Uniform_distribution_error)
                 Uniform_distribution_error  = std::abs(Uniform_distribution_inter_value - fun(x));
             if (std::abs(Chebyshev_inter_value - fun(x)) > Chebyshev_error)
